78c7a812d6f05fd165757798dc1a7d94a200d430
unknown <aghajanyanalbert@gmail.com>
2023-10-22 19:01:56 +0400

    python

---
+++ project1-hangman.py
@@ -0,0 +1,241 @@
+import random
+
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+
+word_list = [
+'abruptly',
+'absurd',
+'abyss',
+'affix',
+'askew',
+'euouae',
+'exodus',
+'faking',
+'fishhook',
+'fixable',
+'fjord',
+'flapjack',
+'flopping',
+'fluffiness',
+'flyby',
+'foxglove',
+'frazzled',
+'frizzled',
+'fuchsia',
+'funny',
+'gabby',
+'galaxy',
+'galvanize',
+'gazebo',
+'giaour',
+'gizmo',
+'glowworm',
+'glyph',
+'jogging',
+'joking',
+'jovial',
+'joyful',
+'juicy',
+'jukebox',
+'jumbo',
+'kayak',
+'kazoo',
+'keyhole',
+'khaki',
+'kilobyte',
+'kiosk',
+'kitsch',
+'kiwifruit',
+'klutz',
+'knapsack',
+'larynx',
+'lengths',
+'lucky',
+'luxury',
+'lymph',
+'marquis',
+'matrix',
+'megahertz',
+'microwave',
+'mnemonic',
+'mystify',
+'naphtha',
+'nightclub',
+'nowadays',
+'numbskull',
+'nymph',
+'onyx',
+'ovary',
+'oxidize',
+'oxygen',
+'pajama',
+'peekaboo',
+'phlegm',
+'pixel',
+'pizazz',
+'pneumonia',
+'polka',
+'pshaw',
+'psyche',
+'puppy',
+'puzzling',
+'quartz',
+'queue',
+'quips',
+'quixotic',
+'quiz',
+'quizzes',
+'quorum',
+'razzmatazz',
+'rhubarb',
+'rhythm',
+'rickshaw',
+'schnapps',
+'scratch',
+'shiv',
+'snazzy',
+'sphinx',
+'spritz',
+'squawk',
+'staff',
+'strength',
+'strengths',
+'stretch',
+'stronghold',
+'stymied',
+'subway',
+'swivel',
+'syndrome',
+'thriftless',
+'thumbscrew',
+'topaz',
+'transcript',
+'transgress',
+'wheezy',
+'whiskey',
+'whizzing',
+'whomever',
+'wimpy',
+'witchcraft',
+'wizard',
+'woozy',
+'wristwatch',
+'wyvern',
+'xylophone',
+'yachtsman',
+'yippee',
+'yoked',
+'youthful',
+'yummy',
+'zephyr',
+'zigzag',
+'zigzagging',
+'zilch',
+'zipper',
+'zodiac',
+'zombie',
+]
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+print(logo)
+
+chosen_word = random.choice(word_list)
+# print(f"This word is: {chosen_word}")
+
+list_word = []
+game_result = False
+lives = 6
+
+for letter in chosen_word:
+    list_word.append('_')
+
+while not game_result:
+    guess = input('Guess a letter?').lower()
+
+    if guess in list_word:
+        print(f"You have already guessed {guess}")
+
+    if guess not in chosen_word:
+        print(f"You guessed {guess}, that's not in the word. You lose a life.")
+        lives -= 1
+        print(stages[lives])
+        if lives == 0:
+            print("You lose!")
+            break
+
+    for i in range(len(chosen_word)):
+        if chosen_word[i] == guess:
+            list_word[i] = guess
+
+    print(f"New word: {list_word}")
+
+    if '_' not in list_word:
+        game_result = True
+        print("You Win! You guess the word!")
+
+
+
